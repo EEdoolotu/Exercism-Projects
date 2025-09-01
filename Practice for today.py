050544a11a64ea3751b4aa56b2f0604da9f003eb
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-01 15:22:34 +0100

    correction

--- Practice for today.py
+++ Practice for today.py
@@ -58,21 +58,12 @@ def response(hey_bob):
     
 #contionals 2 
 def convert(number):
-    
-    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
-        return "PlingPlangPlong"
-    elif number % 3 == 0 and number % 7 == 0:
-        return "PlingPlong"
-    elif number % 5 == 0 and number % 7 == 0:
-        return "PlangPlong"
-    elif number % 3 == 0 and number % 5 == 0:
-        return "PlingPlang"
-    elif number % 3 == 0:
-        return "Pling"
-    elif number % 5 == 0:
-        return "Plang"
-    elif number % 7 == 0:
-        return "Plong"
-    
-    else:
-        return str(number)
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    return result if result else str(number)
+
