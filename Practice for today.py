2a5c865ca084072d4d5af4766fab710361a86814
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-01 14:23:50 +0100

    leap year function

--- Practice for today.py
+++ Practice for today.py
@@ -6,3 +6,16 @@ def square_root(number):
             return i
 
     raise ValueError("Pls enter a valid whole number")
+
+
+
+#leap year function
+def leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
