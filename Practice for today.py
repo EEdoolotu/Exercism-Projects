e17278e1c544881ab9ab0b569b9853d0f606709d
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-09-01 20:21:34 +0100

    brackets

--- Practice for today.py
+++ Practice for today.py
@@ -100,3 +100,18 @@ def translate(text):
 
     return " ".join(translate_word(w) for w in text.split())
 
+#matching brackets
+def is_paired(input_string):
+    openers = {"(", "{" , "["}
+    matches = {")":"(", "]":"[", "}":"{"}
+    stack = []
+
+    for ch in input_string:
+        if ch in openers:
+            stack.append(ch)
+        elif ch in matches:
+            if not stack or stack[-1] != matches[ch]:
+                return False
+
+            stack.pop()
+    return not stack
