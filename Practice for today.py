ee666702aaca7b4aed2d7235f5ee1bafb4b8bb63
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-01 15:19:16 +0100

    cond commit

--- Practice for today.py
+++ Practice for today.py
@@ -56,3 +56,23 @@ def response(hey_bob):
         return "Whatever."
         
     
+#contionals 2 
+def convert(number):
+    
+    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
+        return "PlingPlangPlong"
+    elif number % 3 == 0 and number % 7 == 0:
+        return "PlingPlong"
+    elif number % 5 == 0 and number % 7 == 0:
+        return "PlangPlong"
+    elif number % 3 == 0 and number % 5 == 0:
+        return "PlingPlang"
+    elif number % 3 == 0:
+        return "Pling"
+    elif number % 5 == 0:
+        return "Plang"
+    elif number % 7 == 0:
+        return "Plong"
+    
+    else:
+        return str(number)
