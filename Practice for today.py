d1a9953059d4d7fff949c334a9ef8fbb257ecdbb
EEdoolotu <131226967+EEdoolotu@users.noreply.github.com>
2025-09-02 15:41:13 +0100

    isbn

--- Practice for today.py
+++ Practice for today.py
@@ -136,3 +136,20 @@ def is_isogram(string):
     return len(set(letters)) == len(letters)
             
         
+#isbn task
+def is_valid(isbn):
+    s = isbn.replace("-", "").replace(" ","")
+    if len(s) != 10:
+        return False
+    if not s[:9].isdigit():
+        return False
+    if not (s[9].isdigit() or s[9] == "X"):
+        return False
+
+    digits = [int(ch) for ch in s[:9]]
+    check = 10 if s[9] == "X" else int(s[9])
+    digits.append(check)
+    total = sum((10 - i) * digits[i] for i in range(10))
+    return total % 11 == 0
+
+    
