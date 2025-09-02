b63bddf4211492e7c1840b53716494e618d953f5
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-09-02 18:44:34 +0100

    darts

--- Practice for today.py
+++ Practice for today.py
@@ -165,3 +165,15 @@ def rotate(text, key):
 
     return result 
     
+#darts 
+def score(x, y):
+    distance = (x **2 + y **2) ** 0.5
+
+    if distance <= 1:
+        return 10
+    elif distance <= 5:
+        return 5
+    elif distance <= 10:
+        return 1
+    else: 
+        return 0
